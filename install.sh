89729ffe381601838d7d38a4082f2112e48a0f4f
Zachary Flight <zachary@grotonbiblechapel.org>
2025-06-12 20:16:15 -0400

    Updated install

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ mv -f ~/Documents/Scripts/pro7sync/* ~/Documents/Scripts
 rm -f ~/Documents/Scripts/readme.md
 rm -rf ~/Documents/Scripts/pro7sync
 chmod +x ~/Documents/Scripts/*.sh
-chmod +x ~/Documents/Scripts/*.command
+chmod +x ~/Documents/Scripts/install.command
+chmod +x ~/Documents/Scripts/pro7adddelete.command
+chmod +x ~/Documents/Scripts/pro7scriptsmanual.command
 
 echo "Scripts Installed"
