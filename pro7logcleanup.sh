b5c06120eeaad259f94d1c078e492644813d228f
Zachary Flight <zachary@grotonbiblechapel.org>
2026-01-03 15:32:16 -0500

    Cleaned up log cleanup file

--- pro7logcleanup.sh
+++ pro7logcleanup.sh
@@ -4,14 +4,13 @@ logdatetime=$(date +%F_%H:%M:%S)
 
 echo "$logdatetime - Running Log Cleanup Now";
 
-#onemonthago=$(date -v-1m +%Y-%m)
+onemonthago=$(date -v-1m +%Y-%m)
 twomonthsago=$(date -v-2m +%Y-%m)
 fileslist=$(ls ~/Sync/ProPresenter_Shared_Content/Logs/)
 
 declare -a files=($fileslist)
 for file in "${files[@]}"
 do
-#  sed -i "" "/$onemonthago/d" ~/Sync/ProPresenter_Shared_Content/Logs/$file
   sed -i "" "/$twomonthsago/d" ~/Sync/ProPresenter_Shared_Content/Logs/$file
 done
 
